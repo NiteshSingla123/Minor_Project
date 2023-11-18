e179213a6169df1ee434ab590b85804d74ca8a6e
Manya <123743851+manya16102003@users.noreply.github.com>
2023-11-18 13:09:44 +0530

    Update minor.cpp

--- minor.cpp
+++ minor.cpp
@@ -1,16 +1,17 @@
-
 #include<iostream>
 #include<bits/stdc++.h>
 #include<string.h>
-
-class Rider{
+using namespace std;
+class Driver
+{
 public:
-string name,cno;
+    string name,cno;
     long long int ano;
     int nos,age,price;
     string origin,destination;
     int stdr,destdr;
-void input()
+
+    void input()
     {
         //Accepting details of customers
         cout<<"Enter the required details..."<<endl;
@@ -35,16 +36,30 @@ void input()
         cout<<endl;
         cout<<"************************************************"<<endl;
     }
+    void display()
+    {
+        //Details displayed
+        cout<<"NAME : "<<name<<endl;
+        cout<<"AGE : "<<age<<endl;
+        cout<<"ORIGIN : "<<origin<<endl;
+        cout<<"DESTINATION : "<<destination<<endl;
+        cout<<"AVAILABLE SEATS : "<<nos<<endl;
+        cout<<"PRICE (per Km) : "<<price<<endl;
+        cout<<endl;
+        cout<<"************************************************"<<endl;
+    }
 };
+
 class Passenger
 {
-	public:
-	string pname;
+public:
+    string pname;
     long long int ano;
     int nop,age,price;
     string origin,destination;
     int stp,destp;
-      void input()
+
+    void input()
     {
         cout<<"Enter the required details..."<<endl;
         cout<<"NAME : ";
@@ -65,24 +80,44 @@ class Passenger
         cout<<"*************************************************"<<endl;
 
     }
+    void display()
+    {
+        cout<<"NAME : "<<pname<<endl;
+        cout<<"AGE : "<<age<<endl;
+        cout<<"ORIGIN : "<<origin<<endl;
+        cout<<"DESTINATION : "<<destination<<endl;
+        cout<<"TOTAL PASSENGERS : "<<nop<<endl;
+        cout<<endl;
+        cout<<"*************************************************"<<endl;
+    }
 };
 int main()
 {
-	Rider R[100];
-	Passenger P[100];
-	int d=0,choice,p=0;
+    Driver D[100];
+    Passenger P[100];
+    string Loc[] = {"COLLEDGE","PONDA","NANDA_KI_CHAUKI","PREMNAGAR","BALLUPUR","CLOCK_TOWER","BUDDHA_CHOCK","DWARIKA","SURVEY_CHOCK","SHASTRADHARA CROSSING","RING_ROAD"};
+    int choice,d=0,p=0,confirmchoice;
+    // d is serial of driver and p is for passenger
+    int flag=0;
+    // flag we are using to check wheather a ride confirm or not
 
+
+    cout<<":::<====WELCOME TO THE RIDE SHARE====>:::"<<endl;
     START:
+        confirmchoice=0,flag=0;
     cout<<"Enter 1 to enter as a DRIVER"<<endl;
     cout<<"Enter 2 to enter as a PASSENGER"<<endl;
     cout<<"Enter 3 to EXIT the program"<<endl;
     cout<<"ENTER YOUR CHOICE : ";
     cin>>choice;
-	switch(choice)
-	{
-		case 1:{
-			
-			R[d].input();
+    cout<<"***********************************************"<<endl;
+    cout<<endl;
+
+    switch(choice)
+    {
+    case 1:
+            {
+                D[d].input();
                     for(int i=0;i<11;i++)
                     {
                          if(D[d].origin.compare(Loc[i]) == 0)
@@ -96,8 +131,9 @@ int main()
                     }
                 d++;
                 goto START;
-		}
-		case 2:
+            }
+
+    case 2:
             {
                 P[p].input();
                     for(int i=0;i<11;i++)
@@ -115,7 +151,7 @@ int main()
 
                 break;
             }
-			case 3:
+    case 3:
         {
             goto EXIT;
         }
@@ -126,8 +162,84 @@ int main()
                 goto START;
             }
     }
+    cout<<"==========> AVAILABLE RIDES <=========="<<endl;
+    if(P[p].stp>P[p].destp)
+    {
+        for(int i=0;i<d;i++)
+        {
+            if(D[i].stdr > D[i].destdr && D[i].nos!=0)
+            {
+                if(P[p].stp<=D[i].stdr && P[p].destp>=D[i].destdr && P[p].nop<=D[i].nos )
+                {
+                    D[i].display();
+                    cout<<"ENTER "<<i+1<<" TO CONFIRM YOUR RIDE WITH "<<D[i].name<<" OR 0 TO TO NOT CONFIRM IT"<<endl;
+                    cout<<"*************************************************"<<endl;
+                    D[i].display();
+                    flag = 1;
 
-	}
+                }
+            }
+
+        }
+    }
+    else
+    {
+        if(P[p].stp<P[p].destp)
+        {
+            for(int i=0;i<d;i++)
+            {
+                if(D[i].stdr < D[i].destdr && D[i].nos!=0)
+                {
+                    if(P[p].stp>=D[i].stdr && P[p].destp<=D[i].destdr && P[p].nop<=D[i].nos )
+                    {
+                        D[i].display();
+                        cout<<"ENTER "<<i+1<<" TO CONFIRM YOUR RIDE WITH "<<D[i].name<<" OR 0 TO TO NOT CONFIRM IT"<<endl;
+                        cout<<"*************************************************"<<endl;
+                        flag = 1;
+
+                    }
+
+                }
+            }
+        }
+    }
+    if(flag == 1)
+    {
+        cin>>confirmchoice;
+        if(confirmchoice != 0)
+        {
+            D[confirmchoice - 1].nos = D[confirmchoice - 1].nos - P[p].nop;
+            cout<<"************************************************"<<endl;
+            cout<<"YOUR RIDE WITH "<<D[confirmchoice-1].name<<" HAS BEN CONFIRMED"<<endl;
+            cout<<"THANK YOU FOR USING RIDESHARE.HAVE A SAFE JOURNEY"<<endl;
+            cout<<"                 DO VISIT AGAIN                 "<<endl;
+            cout<<"************************************************"<<endl;
+        }
+        else
+        {
+            cout<<"************************************************"<<endl;
+            cout<<"         THANK YOU FOR USING RIDESHARE.          "<<endl;
+            cout<<"                 DO VISIT AGAIN                 "<<endl;
+            cout<<"************************************************"<<endl;
+        }
+
+    }
+    else
+    {
+        cout<<"SORRY...!!!!!NO RIDES AVAILABLE"<<endl;
+        cout<<"PLEASE TRY AGIAN";
+        cout<<"*************************************************"<<endl<<endl;
+    }
+
+    goto START;
+    EXIT:
+        cout<<"";
+}
+
+
+
+
+
 
 
 
